69295e232adc575c8bff93228c70a36a92ed6c18
Sykat <sykat12@gmail.com>
2025-03-09 11:49:05 +0000

    added 2 projects

--- src/Pages/Home/Projects/Projects.jsx
+++ src/Pages/Home/Projects/Projects.jsx
@@ -7,6 +7,18 @@ import study_study from "../../../assets/project_images/study_buddy/study_page.j
 import study_user_search from "../../../assets/project_images/study_buddy/user_search.jpeg";
 import study_chat from "../../../assets/project_images/study_buddy/chat_page.jpeg";
 import cityGame from "../../../assets/project_images/city_game/city_game.png";
+import airAdmin from "../../../assets/project_images/airvia/admin.png";
+import airCustomer from "../../../assets/project_images/airvia/customer_creation.png";
+import airReport from "../../../assets/project_images/airvia/generate_report.png";
+import airLogin from "../../../assets/project_images/airvia/login.png";
+import airManager from "../../../assets/project_images/airvia/manager.png";
+import airRefund from "../../../assets/project_images/airvia/refund.png";
+import airBlank from "../../../assets/project_images/airvia/selling_blank.png";
+import airStock from "../../../assets/project_images/airvia/view_own_stock.png";
+import cvCnn from "../../../assets/project_images/computer_vision/cnn.png";
+import cvHogMlp from "../../../assets/project_images/computer_vision/hog_mlp.png";
+import cvHogSvm from "../../../assets/project_images/computer_vision/hog_svm.png";
+import cvPersonal from "../../../assets/project_images/computer_vision/personal.png";
 
 export default function Projects() {
   return (
@@ -21,16 +33,95 @@ export default function Projects() {
           { type: "image", src: study_study },
           { type: "image", src: study_user_search },
           { type: "image", src: study_chat },
+          { type: "youtube", src: "-dFA00eyxpI" },
         ]}
         title={"Study Buddy"}
-        description={`For my final year project, I developed a Flutter application called
+        description={`For my final year project at university, I developed a Flutter application called
             Study Buddy. Designed for university students. This social
             media-inspired app helps students connect and organize in-person
             study sessions. Built using Dart programming language and powered by
             Firebase for backend services, Study Buddy enables users to find
-            study partners, create groups, and enhance their academic network
+            study partners, create groups, send messages and enhance their academic network
             seamlessly`}
         languageTechnology={["Flutter", "Dart", "Firebase"]}
+      ></Project>
+      <Project
+        title={"Computer Vision - Face Covering Detection Models"}
+        linkGitHub={
+          "https://github.com/sykathowlader/computer_vision_mask_detection"
+        }
+        media={[
+          {
+            type: "image",
+            src: cvHogSvm,
+          },
+          {
+            type: "image",
+            src: cvHogMlp,
+          },
+          {
+            type: "image",
+            src: cvCnn,
+          },
+          {
+            type: "image",
+            src: cvPersonal,
+          },
+          {
+            type: "youtube",
+            src: "Ko88Wd4Ttqw",
+          },
+        ]}
+        description={"Computer vision"}
+        languageTechnology={["OpenCv", "Pandas"]}
+      ></Project>
+      <Project
+        linkGitHub={"https://github.com/sykathowlader/air_via_system"}
+        title={"Plane ticket sales system"}
+        media={[
+          {
+            type: "image",
+            src: airLogin,
+          },
+          {
+            type: "image",
+            src: airCustomer,
+          },
+          {
+            type: "image",
+            src: airBlank,
+          },
+          {
+            type: "image",
+            src: airRefund,
+          },
+          {
+            type: "image",
+            src: airStock,
+          },
+          {
+            type: "image",
+            src: airReport,
+          },
+          {
+            type: "image",
+            src: airManager,
+          },
+          {
+            type: "image",
+            src: airAdmin,
+          },
+        ]}
+        description={`
+          In my second year of university, I worked on a group project to build a 
+          plane ticket sales system for a fictional travel agency, Air Via Ltd. 
+          We developed it from scratch, creating use case and class diagrams, 
+          designing the database entity-relationship diagram (ERD), and coding it 
+          with Java and JavaFX. I focused on the class diagram and implementation, 
+          using MySQL for the database. Our project ranked in the top three of our 
+          cohort, meeting all requirements with a sleek, user-friendly GUI.
+          `}
+        languageTechnology={["JavaFx", "Java", "MySQL"]}
       ></Project>
       <Project
         linkGitHub={"https://github.com/sykathowlader/java_platform_game"}
@@ -45,12 +136,12 @@ export default function Projects() {
           },
         ]}
         title={"Java Platform Game"}
-        description={`In my year 1 for the Java module, I developed a game where the main character, 
-          called Hero, collects coins while avoiding fireballs falling from the sky. 
-          The game features three levels with increasing difficulty and uses the City 
-          Physique Engine for 2D physics like collisions and gravity. Despite challenges 
-          like debugging NullPointerExceptions, I'm proud of this project that includes 
-          an interactive menu, sound effects, and background music.`}
+        description={`In my first year Java module, I developed a game featuring a main 
+          character, Hero, who collects coins while dodging fireballs falling from the sky. 
+          The game includes three levels of increasing difficulty and leverages the 
+          City Physique Engine for 2D physics (provided by the University), such as collisions and gravity. 
+          Despite challenges like debugging NullPointerExceptions, I’m proud of this project, 
+          which features an interactive menu, sound effects, and background music.`}
         languageTechnology={["Java"]}
       ></Project>
     </div>
